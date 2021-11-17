ad53ba889659308514fc54a90d89abd0f16cb305
Erwan <erwanletutour.elt@gmail.com>
2021-11-17 13:11:04 +0100

    update README

--- Discord-bot/src/main/java/fr/eletutour/constants/BotConstant.java
+++ Discord-bot/src/main/java/fr/eletutour/constants/BotConstant.java
@@ -3,5 +3,5 @@ package fr.eletutour.constants;
 public class BotConstant {
 
     public static final String PREFIX = "&";
-    public static final String TOKEN = "OTEwMTE3MTM4Mzk0NjczMTYy.YZOKog.bnjN9gdtqHRjgdvFyCsV_-MPJlI";
+    public static final String TOKEN = "";
 }
